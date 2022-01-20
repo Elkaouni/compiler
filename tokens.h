96ce1f901fd87a84c230c8407de4f5d55f97d1a0
Elkaouni <76531172+Elkaouni@users.noreply.github.com>
2022-01-20 21:24:19 +0100

    Update tokens.h

--- tokens.h
+++ tokens.h
@@ -1,62 +1,76 @@
-static char* token_names[] = { "PROGRAM_TOKEN", "REPEAT_TOKEN",
-		"UNTIL_TOKEN" , "CONST_TOKEN" ,	"VAR_TOKEN"   , "BEGIN_TOKEN" ,
-		"ELSE_TOKEN"  , "END_TOKEN"   ,	"IF_TOKEN"    , "THEN_TOKEN"  ,
-		"WHILE_TOKEN" ,	"DO_TOKEN"    ,	"FOR_TOKEN"   ,	"OF_TOKEN"    ,
-		"READ_TOKEN"  ,	"CASE_TOKEN"  ,	"WRITE_TOKEN" ,	"STRING_TOKEN",
-		"ID_TOKEN"    ,	"NUM_TOKEN"   ,	"INTO_TOKEN"  ,	"DOWNTO_TOKEN",
-		"PV_TOKEN"    ,	"PT_TOKEN"    ,	"DPT_TOKEN"   ,	"VIR_TOKEN"   ,
-		"AFF_TOKEN"   ,	"INF_TOKEN"   ,	"INFEG_TOKEN" ,	"SUP_TOKEN"   ,
-		"SUPEG_TOKEN" ,	"DIFF_TOKEN"  ,	"PO_TOKEN"    ,	"PF_TOKEN"    ,
-		"PLUS_TOKEN"  ,	"MINUS_TOKEN" ,	"MULT_TOKEN"  ,	"EG_TOKEN"    ,
-		"DIV_TOKEN"   ,	"INTLIT_TOKEN",	"EOF_TOKEN"   , "ERR_TOKEN"
+static char* token_names[] = { "MAIN_TOKEN", "TYSTRING_TOKEN",
+		"CONST_TOKEN"  , "TYNUM_TOKEN" , "NULL_TOKEN"  , "INTLIT_TOKEN",
+		"DECLARE_TOKEN", "END_TOKEN"   , "BEGIN_TOKEN" , "RETURN_TOKEN",
+        "TRUE_TOKEN"   , "FALSE_TOKEN" , "IF_TOKEN"    , "ELSE_TOKEN"  ,
+        "ELIF_TOKEN"   , "WHILE_TOKEN" , "DO_TOKEN"    , "FOR_TOKEN"   ,
+		"SCAN_TOKEN"   , "IN_TOKEN"    , "PRINT_TOKEN" , "STRING_TOKEN",
+		"ID_TOKEN"     , "NUM_TOKEN"   , "DESC_TOKEN"  , "ASC_TOKEN"   ,
+
+		"PV_TOKEN"     , "PT_TOKEN"    , "DPT_TOKEN"   , "VIR_TOKEN"   ,
+		"AFF_TOKEN"    , "INF_TOKEN"   , "INFEG_TOKEN" , "SUP_TOKEN"   ,
+		"SUPEG_TOKEN"  , "DIFF_TOKEN"  , "PO_TOKEN"    , "PF_TOKEN"    ,
+		"PLUS_TOKEN"   , "MINUS_TOKEN" , "MULT_TOKEN"  , "MOD_TOKEN"   ,
+		"EG_TOKEN"     , "DIV_TOKEN"   , "AO_TOKEN"    , "AF_TOKEN"    ,
+		"CO_TOKEN"     , "CF_TOKEN"    , "COMM_TPKEN"  , "EOF_TOKEN"   ,
+		"ERR_TOKEN"
     };
 
 typedef struct
 {
 	enum
 	{
-		PROGRAM_TOKEN,
-		REPEAT_TOKEN,
-		UNTIL_TOKEN,
-		CONST_TOKEN,
-		VAR_TOKEN,
-		BEGIN_TOKEN,
-		ELSE_TOKEN,
-		END_TOKEN,
-		IF_TOKEN,
-		THEN_TOKEN,
-		WHILE_TOKEN,
-		DO_TOKEN,
-		FOR_TOKEN,
-		OF_TOKEN,
-		READ_TOKEN,
-		CASE_TOKEN,
-		WRITE_TOKEN,
+		MAIN_TOKEN  ,
+		TYSTRING_TOKEN,
+		CONST_TOKEN ,
+		TYNUM_TOKEN ,
+		NULL_TOKEN  ,
+		INTLIT_TOKEN,
+		DECLARE_TOKEN ,
+		END_TOKEN   ,
+		BEGIN_TOKEN ,
+		RETURN_TOKEN,
+        TRUE_TOKEN  ,
+        FALSE_TOKEN ,
+        IF_TOKEN    ,
+        ELSE_TOKEN  ,
+        ELIF_TOKEN  ,
+        WHILE_TOKEN ,
+        DO_TOKEN    ,
+        FOR_TOKEN   ,
+		SCAN_TOKEN  ,
+		IN_TOKEN    ,
+		PRINT_TOKEN ,
 		STRING_TOKEN,
-		ID_TOKEN,
-		NUM_TOKEN,
-		INTO_TOKEN,
-		DOWNTO_TOKEN,
-		PV_TOKEN,
-		PT_TOKEN,
-		DPT_TOKEN,
-		VIR_TOKEN,
-		AFF_TOKEN,
-		INF_TOKEN,
-		INFEG_TOKEN,
-		SUP_TOKEN,
-		SUPEG_TOKEN,
-		DIFF_TOKEN,
-		PO_TOKEN,
-		PF_TOKEN,
-		PLUS_TOKEN,
-		MINUS_TOKEN,
-		MULT_TOKEN,
-		EG_TOKEN,
-		DIV_TOKEN,
-		INTLIT_TOKEN, // NUMBERS
-		EOF_TOKEN,
-        ERR_TOKEN
+		ID_TOKEN    ,
+		NUM_TOKEN   ,
+		DESC_TOKEN  ,
+        ASC_TOKEN   ,
+
+		PV_TOKEN    , // ;
+		PT_TOKEN    , // .
+		DPT_TOKEN   , // :
+		VIR_TOKEN   , // ,
+		AFF_TOKEN   , // =
+		INF_TOKEN   , // <
+		INFEG_TOKEN , // <=
+		SUP_TOKEN   , // >
+		SUPEG_TOKEN , // >=
+		DIFF_TOKEN  , // !=
+		PO_TOKEN    , // (
+		PF_TOKEN    , // )
+		PLUS_TOKEN  , // +
+		MINUS_TOKEN , // -
+		MULT_TOKEN  , // *
+		MOD_TOKEN   , // %
+		EG_TOKEN    , // ==
+		DIV_TOKEN   , // /
+		AO_TOKEN    , // {
+		AF_TOKEN    , // }
+		CO_TOKEN    , // /*
+		CF_TOKEN    , // */
+		COMM_TOKEN  ,
+		EOF_TOKEN   ,
+		ERR_TOKEN
 	} type;
 	char *value;
 } Token;
